14e806a07997e43bd793e863edb11f3017cdf876
m1Myp <55120919+m1Myp@users.noreply.github.com>
2021-09-29 21:36:11 +0700

    Update HeapSortTests.java

--- Task_1_1_1/src/test/java/HeapSortTests.java
+++ Task_1_1_1/src/test/java/HeapSortTests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import org.junit.Test;
 
 public class HeapSortTests {
@@ -7,28 +8,32 @@ public class HeapSortTests {
     @Test
     public void simpleTests() {
         int[] arr1 = {4, 5, 1, 0, -5};
-        int[] sortedArr1 = {-5, 0, 1, 4, 5};
-        HeapSort.heapSort(arr1, arr1.length);
+        int[] sortedArr1 = arr1.clone();
+        Arrays.sort(sortedArr1);
+        HeapSort.heapSort(arr1);
         assertArrayEquals(sortedArr1, arr1);
         int[] arr2 = {99, 23, -38, -93, 0, 0, -67, 8, 39, -34};
-        int[] sortedArr2 = {-93, -67, -38, -34, 0, 0, 8, 23, 39, 99};
-        HeapSort.heapSort(arr2, arr2.length);
+        int[] sortedArr2 = arr2.clone();
+        Arrays.sort(sortedArr2);
+        HeapSort.heapSort(arr2);
         assertArrayEquals(sortedArr2, arr2);
     }
 
     @Test
-    public void testEmptyArr() {
+    public void testEmptyArray() {
         int[] emptyArr = {};
-        int[] emptyArrSorted = {};
-        HeapSort.heapSort(emptyArr, 0);
+        int[] emptyArrSorted = emptyArr.clone();
+        Arrays.sort(emptyArrSorted);
+        HeapSort.heapSort(emptyArr);
         assertArrayEquals(emptyArr, emptyArrSorted);
     }
 
     @Test
-    public void testLimIntVal() {
+    public void testLimitsOfIntValues() {
         int[] arr = {3, 1, 2, Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE};
-        int[] sortedArr = {Integer.MIN_VALUE, Integer.MIN_VALUE, 1, 2, 3, Integer.MAX_VALUE, Integer.MAX_VALUE};
-        HeapSort.heapSort(arr, arr.length);
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        HeapSort.heapSort(arr);
         assertArrayEquals(arr, sortedArr);
     }
 }
