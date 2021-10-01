bd6574c3791c72369b7546de26d5c8234eff6bb3
m1Myp <55120919+m1Myp@users.noreply.github.com>
2021-10-01 15:07:16 +0700

    Сделал методСурс (крутой)

--- Task_1_1_1/src/test/java/ru/nsu/fit/oop/lab1/HeapSortTests.java
+++ Task_1_1_1/src/test/java/ru/nsu/fit/oop/lab1/HeapSortTests.java
@@ -1,43 +1,31 @@
 package ru.nsu.fit.oop.lab1;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
+
 
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class HeapSortTests {
-
-
-    @Test
-    public void testEmptyArray() {
-        int[] emptyArr = {};
-        int[] emptyArrSorted = emptyArr.clone();
-        Arrays.sort(emptyArrSorted);
-        HeapSort.heapSort(emptyArr);
-        assertArrayEquals(emptyArr, emptyArrSorted);
-    }
-
-    @Test
-    public void testLimitsOfIntValues() {
-        int[] arr = {3, 1, 2, Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE};
-        int[] sortedArr = arr.clone();
-        Arrays.sort(sortedArr);
-        HeapSort.heapSort(arr);
-        assertArrayEquals(arr, sortedArr);
-    }
-
     @ParameterizedTest
-    @ValueSource(strings = {"4 5 1 0 -5", "99 23 -38 -93 0 0 -67 8 39 -34"})
-    public void simpleTests(String str) {
-        int[] arr = Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] sortedArr = arr.clone();
+    @MethodSource ("allTests")
+    public void simpleTests(int[] arr) {
+       int[] sortedArr = arr.clone();
         Arrays.sort(sortedArr);
         HeapSort.heapSort(arr);
         assertArrayEquals(sortedArr, arr);
     }
 
+    private static Stream<int[]> allTests() {
+        return Stream.of(
+                new int[]{4, 5, 1, 0, -5},
+                new int[]{99, 23, -38, -93, 0, 0, -67, 8, 39, -34},
+                new int[]{3, 1, 2, Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE},
+                new int[]{}
+        );
+    }
 }
