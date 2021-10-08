980e914762dfcfd9d73fdaf5d30669591f205f35
m1Myp <55120919+m1Myp@users.noreply.github.com>
2021-10-08 18:23:16 +0700

    Reader, normal read (commented code without prep work)

--- src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
+++ src/main/java/ru/nsu/fit/oop/lab2/SubstringSearch.java
@@ -1,9 +1,10 @@
 package ru.nsu.fit.oop.lab2;
 
-import java.io.*;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
-import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 import static java.lang.Integer.max;
 
@@ -15,7 +16,7 @@ public class SubstringSearch {
      * @param len len of array of char we want to find
      * @return array of length of the longest native substring prefix
      */
-    private static int[] prefixFunction(String str, int len){
+     private static int[] prefixFunction(String str, int len){
         int[] values = new int[str.length()];
         for(int i = 0; i < len; ++i) values[i] = 0;
         for(int i = len; i < str.length(); ++i){
@@ -31,25 +32,45 @@ public class SubstringSearch {
     /**
      * Finds the indices of the occurrence of a substring {@param searchParam} in {@param file}
      *
-     * @param fileName Name of file where we want to search occurrence
+     * @param @param file InputStreamReader where we want to search occurrence
      * @param searchParam array of char we want to find
      * @return array of indices of the occurrences. Count from 0
      * @throws IOException if some problems with a reading of the file
      */
-    public static ArrayList<Integer> KMPSearch(String fileName, String searchParam) throws IOException {
-        if (searchParam == null) {
-            throw new NullPointerException("SearchParam is null");
-        }
+        public static ArrayList<Integer> KMPSearch(Reader file, String searchParam) throws IOException {
+            if (file == null) {
+                throw new NullPointerException("File is null");
+            }
+            if (searchParam == null) {
+                throw new NullPointerException("SearchParam is null");
+            }
 
-        ArrayList<Integer> indexes = new ArrayList<>();
-        String text = new String(Files.readAllBytes(Path.of(fileName)));
-        int[] prefixFunc = prefixFunction(searchParam+'#'+text, searchParam.length());
+            ArrayList<Integer> indexes = new ArrayList<>();
+            String text;
+            try (BufferedReader br = new BufferedReader(file)) {
+                text = br.lines().collect(Collectors.joining("\n"));
+            }
+            int[] prefixFunc = prefixFunction(searchParam + '#' + text, searchParam.length());
 
-        for(int i = 0; i <= text.length(); ++i){
-            if(prefixFunc[i+searchParam.length()] >= searchParam.length())
-                indexes.add(i-searchParam.length());
+            for (int i = 0; i <= text.length(); ++i) {
+                if (prefixFunc[i + searchParam.length()] >= searchParam.length())
+                    indexes.add(i - searchParam.length());
+            }
+        /* without any preparation
+        String str = searchParam+'#'+text;
+        int[] values = new int[str.length()];
+        for(int i = 0; i <= searchParam.length(); ++i) values[i] = 0;
+        for(int i = searchParam.length()+1; i < str.length(); ++i){
+            int j = 0;
+            while(i + j < str.length() && str.charAt(j) == str.charAt(i+j)){
+                values[i+j] = max(values[i+j], j + 1);
+                ++j;
+            }
+            if(values[i]>=searchParam.length())
+                indexes.add(i-2*searchParam.length());
         }
-        return indexes;
-    }
+        */
+            return indexes;
+        }
 
 }
