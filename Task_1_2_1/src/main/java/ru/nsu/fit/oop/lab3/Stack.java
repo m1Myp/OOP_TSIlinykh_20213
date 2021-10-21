443a4fd53afbf39f4edd12f77b379b5022885f61
m1Myp <55120919+m1Myp@users.noreply.github.com>
2021-10-21 21:03:47 +0700

    Добавление Array.NewInstans, pushStack и мелкие правки

--- Task_1_2_1/src/main/java/ru/nsu/fit/oop/lab3/Stack.java
+++ Task_1_2_1/src/main/java/ru/nsu/fit/oop/lab3/Stack.java
@@ -1,50 +1,47 @@
 package ru.nsu.fit.oop.lab3;
 
+import java.lang.reflect.Array;
 import java.util.EmptyStackException;
 
 
 public class Stack <T>{
 
-    private int maxSize;
     private T[] arr;
     private int id;
+    private Class<T> stackClass;
 
 
     @SuppressWarnings("unchecked")
-    public Stack (int maxSize)
+    public Stack (int maxSize, Class<T> type)
     {
-        this.maxSize = maxSize;
-        arr = (T[]) new Object[maxSize];
+        stackClass = type;
+        arr = (T[]) Array.newInstance(stackClass, maxSize);
         id = 0;
     }
 
     /**
-     * amount getter
+     * size getter
      * @return Stack.AmountOfElems
      */
     public int getAmount()
     {
         return id;
     }
-    /**
-     * increasing the maximum number of elements in the stack and make array with this length, copy prev arr to this
-     */
     @SuppressWarnings("unchecked")
     private void realloc()
     {
-        T[] newArray = (T[]) new Object[maxSize * 2];
+        T[] newArray = (T[]) Array.newInstance(stackClass, arr.length * 2);
         System.arraycopy(arr, 0, newArray, 0, id);
         arr = newArray;
-        maxSize *=2;
     }
 
     /**
-     * adding to the Stack and realloc array if needed
+     * adding elem to the Stack and realloc array if needed
      * @param elem
      */
     public void push(T elem)
     {
-        if (id == maxSize)
+        if (id == arr.length)
             realloc();
 
         arr[id++] = elem;
@@ -71,7 +68,7 @@ public class Stack <T>{
      */
     public Stack<T> popStack(int count) throws EmptyStackException
     {
-        Stack <T> returnStack = new Stack<T>(count);
+        Stack <T> returnStack = new Stack<>(count, stackClass);
         if (id == 0 || id < count)
             throw new EmptyStackException();
         else {
@@ -83,5 +80,20 @@ public class Stack <T>{
             }
         }
         return returnStack;
+    }
+
+    /**
+     * adding elem from the one Stack to another Stack and realloc array if needed
+     * @param pushingStack
+     */
+    public void pushStack(Stack<T> pushingStack)
+    {
+        int numOfElemsInPushingStack = pushingStack.id;
+        for(int i = 0; i < numOfElemsInPushingStack; ++i) {
+            if (id == arr.length)
+                realloc();
+
+            arr[id++] = pushingStack.arr[i];
+        }
     }
 }
